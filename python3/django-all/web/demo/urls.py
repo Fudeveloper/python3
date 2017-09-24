19e1a8681036f1ac01188f8acbf569d4831cdf31
1178981326@qq.com <wo951127>
2017-09-24 19:19:56 +0800

    py

--- python3/django-all/web/demo/urls.py
+++ python3/django-all/web/demo/urls.py
@@ -2,6 +2,36 @@ from django.conf.urls import url
 from demo import views
 
 urlpatterns = [
+    # 主页
     url(r'^$', views.index, name='index'),
+    url(r'^index.html$', views.index, name='index'),
+
+    # 动力监控
+    url(r'^switch/$', views.switch, name='switch'),
+    url(r'^distribution_box/$', views.distribution_box, name='distribution_box'),
+    url(r'^dynamo/$', views.dynamo, name='dynamo'),
+
+    # 环境监控
+    url(r'^exhaust/$', views.exhaust, name='exhaust'),
+    url(r'^fresh_air/$', views.fresh_air, name='fresh_air'),
+    url(r'^humiture/$', views.humiture, name='humiture'),
+    url(r'^leakage/$', views.leakage, name='leakage'),
+    url(r'^lighting/$', views.lighting, name='lighting'),
+    url(r'^protection/$', views.protection, name='protection'),
+
+    # 产品管理
+    url(r'^archives/$', views.archives, name='archives'),
+    url(r'^order_status/$', views.order_status, name='order_status'),
+    url(r'^plan/$', views.plan, name='plan'),
+    url(r'^tech/$', views.tech, name='tech'),
+
+
+
+
+
+    url(r'^base/$', views.base, name='base'),
+    url(r'^product/$', views.product, name='product'),
+    url(r'^craftwork/$', views.craftwork, name='craftwork'),
+
 
 ]
--- python3/django-all/web/demo/views.py
+++ python3/django-all/web/demo/views.py
@@ -1,6 +1,83 @@
 from django.shortcuts import render
 
+
 # Create your views here.
-# 请求首页
+# 首页
 def index(request):
     return render(request, 'demo/index.html')
+
+
+# 动力监控
+def switch(request):
+    return render(request, 'demo/machine/switch.html')
+
+
+def distribution_box(request):
+    return render(request, 'demo/machine/distribution_box.html')
+
+
+def dynamo(request):
+    return render(request, 'demo/machine/dynamo.html')
+
+
+# 环境监控
+
+def exhaust(request):
+    return render(request, 'demo/envir/exhaust.html')
+
+
+def fresh_air(request):
+    return render(request, 'demo/envir/fresh_air.html')
+
+
+def humiture(request):
+    return render(request, 'demo/envir/humiture.html')
+
+
+def leakage(request):
+    return render(request, 'demo/envir/leakage.html')
+
+
+def lighting(request):
+    return render(request, 'demo/envir/lighting.html')
+
+
+def protection(request):
+    return render(request, 'demo/envir/protection.html')
+
+
+# 产品管理
+def archives(request):
+    return render(request, 'demo/product/archives.html')
+
+
+def order_status(request):
+    return render(request, 'demo/product/order_status.html')
+
+
+def plan(request):
+    return render(request, 'demo/product/plan.html')
+
+
+def tech(request):
+    return render(request, 'demo/product/tech.html')
+
+
+
+
+
+
+
+
+
+def craftwork(request):
+    return render(request, 'demo/base_product2.html')
+
+
+def base(request):
+    return render(request, 'demo/base.html')
+
+
+
+def product(request):
+    return render(request, 'demo/base_product.html')
